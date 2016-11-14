c31a3bbb674d91f64a8414cc8417a6a13b8c8523
kunyanx <kunx.yan@intel.com>
2016-11-14 10:58:22 -0500

    to be continue

--- socket/getaddrinfo.c
+++ socket/getaddrinfo.c
@@ -3,17 +3,27 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 int main(int argc,char *argv)
 {
     const char *p ="not.baidu.com";
     struct addrinfo add_host;
     struct addrinfo *add;
     int ret;
-    ret = getaddrinfo(p,NULL,NULL,&add);
+    struct addrinfo hints;
+    char *s;
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;
+    ret = getaddrinfo(p,"http",&hints,&add);
     if(ret != 0){
        fprintf(stderr,"getaddrinfo:%s\n",gai_strerror(ret));
        exit(0);
     }
+    s = inet_ntoa(add->ai_addr->sin_addr);
+    printf("%s\n",s);
     printf("the end\n");
     exit(0);
 }
