26af1e0d2a26dd7825255da6de3796d4fefade0f
sam <samyankun@gmail.com>
2017-01-02 23:03:30 +0800

    update

--- modules/sam_rm.c
+++ modules/sam_rm.c
@@ -2,25 +2,29 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include "/home/sam/fork/linux/fs/proc/internal.h"
+#include <linux/seq_file.h>
+#include <asm/uaccess.h>
 struct proc_dir_entry *proc_entry;
 
 #if 1
 ssize_t proc_read(struct file *file, char __user *buf, size_t size, loff_t *ppos)
 {
-	char data[20] ="sam";
-	simple_read_from_buffer(buf, size, ppos, data, 3);
-
-	return 0;
+	//char data[3] ="s";
+	signed char data = 'a';	
+	//simple_read_from_buffer(buf, size, ppos, data, 3);
+	copy_to_user(buf,&data,1);
+	return 1;
 }
 #endif
 static struct file_operations proc_fops = {
-.read = proc_read,
+	.read = proc_read,
+	//.read = seq_read,
 };
-static int __init  hello_init(void)
+static int __init hello_init(void)
 {
 	printk("hello,I am coming\n");
-	proc_entry = proc_create("sam_rm",777,NULL,&proc_fops);
-	proc_entry->proc_fops = &proc_fops;
+	proc_entry = proc_create("sam_rm",0,NULL,&proc_fops);
+	//proc_entry->proc_fops = &proc_fops;
 	return 0;
 
 }
