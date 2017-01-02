3389fd8a5aa84090ca078bed24e4dd19ce37ba69
sam <samyankun@gmail.com>
2017-01-02 22:13:12 +0800

    update

--- modules/sam_rm.c
+++ modules/sam_rm.c
@@ -1,9 +1,26 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
+#include "/home/sam/fork/linux/fs/proc/internal.h"
+struct proc_dir_entry *proc_entry;
 
+#if 1
+ssize_t proc_read(struct file *file, char __user *buf, size_t size, loff_t *ppos)
+{
+	char data[20] ="sam";
+	simple_read_from_buffer(buf, size, ppos, data, 3);
+
+	return 0;
+}
+#endif
+static struct file_operations proc_fops = {
+.read = proc_read,
+};
 static int __init  hello_init(void)
 {
 	printk("hello,I am coming\n");
+	proc_entry = proc_create("sam_rm",777,NULL,&proc_fops);
+	proc_entry->proc_fops = &proc_fops;
 	return 0;
 
 }
@@ -11,6 +28,7 @@ static void __exit hello_exit(void)
 {
 	printk("hello,I am leaving\n");
 }
-MODULE_LICENSE("Dual BSD/GPL");
 module_init(hello_init);
 module_exit(hello_exit);
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("sam");
