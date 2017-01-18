d479aa9d945602b92a13aebff19dfd09f6e39ccd
Sam <samyankun@gmail.com>
2017-01-18 15:00:13 +0800

    add email Signed-off-by:Kun Yan <kunx.yan@intel.com>

--- shell/command_line.sh
+++ shell/command_line.sh
@@ -4,23 +4,31 @@ usage()
 cat <<-EOF >&2
         I will add its implement
 EOF
-    exit 0
+    #exit 0
 }
 die()
 {
     echo "ERROR -$@"
-    exit 1
+    #exit 1
 }
-while getopts a::h: arg
+while getopts a:b::h arg
 do
     case $arg in
         a) TEST_ARGS_ARRAY+=( "$OPTARG" );;
         h) usage;;
+        b) die "b";;
        \?) die "Invalid Option";;
         :) die "$0:must supply  an argument to -$OPTARG";;
    esac
 done
 for args in ${TEST_ARGS_ARRAY[@]}
 do
-    echo $args
+    case $args in
+        -s)
+            STDIN=1
+            if [ $STDIN -eq 1 ]; then
+                echo "yes"
+            fi
+            ;;
+    esac
 done
