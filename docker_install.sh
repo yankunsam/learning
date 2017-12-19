ddde7b7d8a9fb278be18c80a87ca7568e9f2fdd1
yankunsam <samyankun@gmail.com>
2017-12-19 13:57:50 +0800

    Create docker_install.sh
    help to install docker

---
+++ docker_install.sh
@@ -0,0 +1,13 @@
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce=17.09.0~ce-0~ubuntu
