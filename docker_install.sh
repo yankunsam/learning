ded3d1a631f96fdbffbf36a6280168262caeeb1a
yankunsam <samyankun@gmail.com>
2017-12-19 14:08:31 +0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
